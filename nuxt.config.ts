3112f5437021e4cf57034a5bef6bd6c5016f61be
GustiRafi <gustirafi49@gmail.com>
2024-07-24 10:57:10 +0700

    style: change favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  devServer: {
+    port: 3003
+  },
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
@@ -11,6 +14,11 @@ export default defineNuxtConfig({
   app: {
       head: {
           link: [
+              {
+                  rel: 'icon',
+                  type: 'image/x-icon',
+                  href: '/favicon.png'
+              },
               {
                   rel: 'stylesheet',
                   href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css'
